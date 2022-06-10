52f713856aa39d8a44a1f98fc7cf17af26b7dd99
Maksim <eliott_nu@mail.ru>
2022-06-10 23:31:48 +0900

    Run sequentially

--- AsyncPractice/Assets/Scripts/Shape.cs
+++ AsyncPractice/Assets/Scripts/Shape.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class Shape : MonoBehaviour
@@ -10,6 +11,26 @@ public class Shape : MonoBehaviour
         {
             transform.Rotate(new Vector3(1, 1) * Time.deltaTime * 150);
             yield return null;
+        }
+    }
+    
+    public async void RotateForSecondsAsync(float duration)
+    {
+        var end = Time.time + duration;
+        while (Time.time < end)
+        {
+            transform.Rotate(new Vector3(1, 1) * Time.deltaTime * 150);
+            await Task.Yield();
+        }
+    }
+    
+    public async Task RotateForSecondsTaskAsync(float duration)
+    {
+        var end = Time.time + duration;
+        while (Time.time < end)
+        {
+            transform.Rotate(new Vector3(1, 1) * Time.deltaTime * 150);
+            await Task.Yield();
         }
     }
 }
--- AsyncPractice/Assets/Scripts/ShapeManager.cs
+++ AsyncPractice/Assets/Scripts/ShapeManager.cs
@@ -6,14 +6,32 @@ public class ShapeManager : MonoBehaviour
 
     private void Start()
     {
-        StartTest();
+        //StartCoroutineTest();
+        //StartAsyncTest();
+        StartSequentiallyAsyncTest();
     }
 
-    private void StartTest()
+    private void StartCoroutineTest()
     {
         for (var i = 0; i < _shapes.Length; i++)
         {
             StartCoroutine(_shapes[i].RotateForSeconds(i + 1));
+        }
+    }
+    
+    private void StartAsyncTest()
+    {
+        for (var i = 0; i < _shapes.Length; i++)
+        {
+            _shapes[i].RotateForSecondsAsync(i + 1);
+        }
+    }
+    
+    private async void StartSequentiallyAsyncTest()
+    {
+        for (var i = 0; i < _shapes.Length; i++)
+        {
+            await _shapes[i].RotateForSecondsTaskAsync(i + 1);
         }
     }
 }
